f138c14626a6092535924b02e98305e801441204
Josh Evenson <jevenson@outlook.com>
2019-01-14 20:58:55 -0600

    end of 6

---
+++ catch-of-the-day/src/components/Inventory.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Inventory extends Component {
+
+    render() {
+        return (
+            <div className="inventory">Inventory</div>
+        );
+    }
+}
